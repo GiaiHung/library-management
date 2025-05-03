a714659125ab937aeee8ccd172e5623396c8fe0b
mark <hunggiaitruong288@gmail.com>
2025-05-03 19:31:20 +0700

    Setup API gateway

---
+++ employeeservice/src/main/java/com/giaihung/employeeservice/config/OpenApiConfig.java
@@ -0,0 +1,31 @@
+package com.giaihung.employeeservice.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Employee service",
+                description = "Library Management Project - Employee Service",
+                contact = @Contact(
+                        name = "Truong Giai Hung",
+                        email = "giaihung@gmail.com"
+                ),
+                version = "0.0.0.1"
+        ),
+        servers = {
+                @Server(
+                        url = "http://localhost:8080",
+                        description = "DEV Environment"
+                ),
+                @Server(
+                        url = "employee-service.prod.com",
+                        description = "PROD Environment"
+                )
+        }
+)
+public class OpenApiConfig {
+
+}
--- employeeservice/src/main/java/com/giaihung/employeeservice/query/controller/EmployeeQueryController.java
+++ employeeservice/src/main/java/com/giaihung/employeeservice/query/controller/EmployeeQueryController.java
@@ -4,7 +4,10 @@ import com.giaihung.employeeservice.query.model.EmployeeDTO;
 import com.giaihung.employeeservice.query.model.EmployeeSearchCriteria;
 import com.giaihung.employeeservice.query.queries.GetAllEmployeesQuery;
 import com.giaihung.employeeservice.query.queries.GetDetailEmployeeQuery;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.axonframework.messaging.responsetypes.ResponseTypes;
 import org.axonframework.queryhandling.QueryGateway;
 import org.springframework.http.ResponseEntity;
@@ -16,22 +19,27 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/employees")
 @RequiredArgsConstructor
+@Tag(name = "Employee Query", description = "Full CRUD Management for Employee Entity")
+@Slf4j
 public class EmployeeQueryController {
-  private final QueryGateway queryGateway;
+    private final QueryGateway queryGateway;
 
-  @GetMapping
-  public ResponseEntity<List<EmployeeDTO>> getEmployees(@ModelAttribute EmployeeSearchCriteria criteria) {
-    List<EmployeeDTO> employees = queryGateway.query(
-            new GetAllEmployeesQuery(criteria.getIsDiscipline() != null && criteria.getIsDiscipline()),
-            ResponseTypes.multipleInstancesOf(EmployeeDTO.class)).join();
-    return ResponseEntity.ok(employees);
-  }
+    @GetMapping
+    @Operation(summary = "Get All Employees", description = "Get All Employees with IS DISCIPLINE filter")
+    public ResponseEntity<List<EmployeeDTO>> getEmployees(@ModelAttribute EmployeeSearchCriteria criteria) {
+        log.info("Calling Get All Employees");
+        List<EmployeeDTO> employees = queryGateway.query(
+                new GetAllEmployeesQuery(criteria.getIsDiscipline() != null && criteria.getIsDiscipline()),
+                ResponseTypes.multipleInstancesOf(EmployeeDTO.class)).join();
+        return ResponseEntity.ok(employees);
+    }
 
-  @GetMapping("/{employeeId}")
-  public ResponseEntity<EmployeeDTO> getEmployee(@PathVariable("employeeId") String employeeId) {
-    EmployeeDTO employee = queryGateway.query(
-            new GetDetailEmployeeQuery(employeeId),
-            ResponseTypes.instanceOf(EmployeeDTO.class)).join();
-    return ResponseEntity.ok(employee);
-  }
+    @Operation(summary = "Get Employee Details", description = "Get Employee Details")
+    @GetMapping("/{employeeId}")
+    public ResponseEntity<EmployeeDTO> getEmployee(@PathVariable("employeeId") String employeeId) {
+        EmployeeDTO employee = queryGateway.query(
+                new GetDetailEmployeeQuery(employeeId),
+                ResponseTypes.instanceOf(EmployeeDTO.class)).join();
+        return ResponseEntity.ok(employee);
+    }
 }
