5a0ea5fcd7b29c6f52a09ba1001c6d4c920c943b
mark <hunggiaitruong288@gmail.com>
2025-05-03 20:47:14 +0700

    Rate limiter with Spring Cloud API and Redis

--- apigateway/src/main/java/com/giaihung/apigateway/ApigatewayApplication.java
+++ apigateway/src/main/java/com/giaihung/apigateway/ApigatewayApplication.java
@@ -3,6 +3,9 @@ package com.giaihung.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -10,6 +13,12 @@ public class ApigatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApigatewayApplication.class, args);
+	}
+
+	@Bean
+	public KeyResolver keyResolver() {
+		// Differentiate each user by using user address
+		return exchange -> Mono.just(exchange.getRequest().getRemoteAddress().getAddress().getHostAddress());
 	}
 
 }
