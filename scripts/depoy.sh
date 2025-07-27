adce8c2e8269ecb1941081e4344b6e0a50841dcd
Mark <hung.truong@floatingcube.com>
2025-07-27 14:15:27 +0700

    Update GitHub workflow to upload new code to EC2 Instance

---
+++ scripts/depoy.sh
@@ -0,0 +1,15 @@
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+echo "Deploying with branch: $BRANCH"
+
+# shellcheck disable=SC2164
+cd ~/app/library-management/
+
+git fetch -a
+git checkout $BRANCH
+git pull
+
+docker compose -f docker-compose.yml down
+docker compose -f docker-compose.yml pull discoveryserver
+docker compose -f docker-compose.yml up -d discoveryserver
+docker system prune -af
